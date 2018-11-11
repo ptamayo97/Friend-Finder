a9c4fa42d52c2a9bae9e246374c261cfd74159ee
ptamayo <ptamayo97@yahoo.com>
2018-11-10 23:38:06 -0800

    fixed an issue

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,7 @@ var friendsData = require("../data/friends.js");
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
-        res.json(userData);
+        res.json(friendsData);
     });
 
     app.post("/api/friends", function(req, res) {
